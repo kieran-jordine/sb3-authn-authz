83eef08b505dc52bc79a9e7fb1fc4af51806f55f
kjordine <kieranjordine@gmail.com>
2023-02-06 10:24:15 -0500

    initial commit

---
+++ src/main/java/com/kyzen/authnauthz/AuthnAuthzApplication.java
@@ -0,0 +1,13 @@
+package com.kyzen.authnauthz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnAuthzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthnAuthzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyzen/authnauthz/AuthnAuthzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyzen.authnauthz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthnAuthzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
